d6a6f1fa7ee8a24a546f14f7da49cf78b09925f6
Ross Zurowski <ross@rosszurowski.com>
2018-08-10 09:48:45 -0700

    Update tests

--- default.test.js
+++ default.test.js
@@ -6,6 +6,11 @@ describe('default', () => {
     expect(rules).toMatchSnapshot();
   });
 
+  it('generates border classes correctly', () => {
+    const rules = kit({ borderWidths: { none: '0', thin: '1px', thick: '2px' } });
+    expect(rules).toMatchSnapshot();
+  });
+
   it('generates verbose classes correctly', () => {
     const rules = kit({ verboseClasses: true });
     expect(rules).toMatchSnapshot();
